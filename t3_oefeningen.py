871cb261e1a77ed4d7898399dc412d05654f51fe
SmeetsSimon <simonsmeets99@gmail.com>
2021-04-19 14:26:25 +0200

    hey

--- t3_oefeningen.py
+++ t3_oefeningen.py
@@ -1,17 +1,27 @@
-def bmi ():
-    resultaat = kg/lengte²
+def bmi (gewicht, lengte):
+    resultaat = gewicht/lengte**2
+    t"return resultaat
 
-    return 0
-def omtrek_cirkel ():
+def omtrek_cirkel (r=4):
+    resultaat = 2*r*3.14
+    return resultaat
 
-    return 0
-def oppervlakte_cirkel ():
-    return 0
-def oppervlakte_vierkant ():
+def oppervlakte_cirkel (r=2):
+    resultaat = 3.14 * r**2
+    return resultaat
 
-    return 0
-def volume_cilinder ():
 
-    return 0
-def volume_kubus ():
-    return 0
+def oppervlakte_vierkant (zijde=2):
+    resultaat = zijde*zijde
+    return resultaat
+
+
+def volume_cilinder (r=2, h=4):
+    resultaat = r*r*3.14*h
+    return resultaat
+
+
+def volume_kubus (zijde=2):
+    resultaat = zijde*zijde*zijde
+    return resultaat
+
